eedabb38df11f63d4b8a5297a6059b4343c72298
David Siegel djsiegel@gmail.com <Unknown>
2008-01-03 17:34:46 -0600

    Locate command is now case insensitive

--- Do.Addins/src/Do.Universe/LocateCommand.cs
+++ Do.Addins/src/Do.Universe/LocateCommand.cs
@@ -64,7 +64,7 @@ namespace Do.Universe
 			
 			locate = new System.Diagnostics.Process ();
 			locate.StartInfo.FileName = "locate";
-			locate.StartInfo.Arguments = string.Format ("-n {0} {1}", maxResults, query);
+			locate.StartInfo.Arguments = string.Format ("-i -n {0} {1}", maxResults, query);
 			locate.StartInfo.RedirectStandardOutput = true;
 			locate.StartInfo.UseShellExecute = false;
 			try {
