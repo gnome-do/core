09eeff1dfedf0b5155c93a55bb95f87c34807448
David Siegel <dave@ja7gggggggg9>
2008-02-20 21:01:07 -0500

    Split RelevanceProvider.cs into two files (one for HistogramRelevanceProvider).

--- Do/src/Do.Core/RelevanceProvider.cs
+++ Do/src/Do.Core/RelevanceProvider.cs
@@ -21,9 +21,6 @@
 using System;
 using System.Collections.Generic;
 
-using System.IO;
-using System.Threading;
-
 using Do.Universe;
 
 namespace Do.Core
@@ -118,7 +115,6 @@ namespace Do.Core
 			return false;
 		}
 
-
 		public virtual void IncreaseRelevance (DoObject r, string match, DoObject other)
 		{
 		}
@@ -135,191 +131,6 @@ namespace Do.Core
 		public virtual bool CanBeFirstResultForKeypress (DoObject r, char a)
 		{
 			return IObjectBelongsInFirstResultsForKeypress (r, a);
-		}
-	}
-	
-	class HistogramRelevanceProvider : RelevanceProvider {
-		
-		int maxActionHits, maxItemHits;
-		Dictionary<int, int> itemHits, actionHits;
-		
-		Timer serializeTimer;
-		const int SerializeInterval = 1*60;
-		
-		public HistogramRelevanceProvider ()
-		{
-			maxActionHits = maxItemHits = 0;
-			itemHits = new Dictionary<int,int> ();
-			actionHits = new Dictionary<int,int> ();
-
-			Deserialize ();
-			
-			// Serialize every few minutes.
-			serializeTimer = new Timer (OnSerializeTimer);
-			serializeTimer.Change (SerializeInterval*1000, SerializeInterval*1000);
-		}
-		
-		protected string DB {
-			get {
-				return "~/.do/relevance2".Replace ("~",
-					Environment.GetFolderPath (Environment.SpecialFolder.Personal));
-			}
-		}
-		
-		private void OnSerializeTimer (object state)
-		{
-			Serialize ();
-		}
-			
-		protected void Deserialize ()
-		{
-			lock (itemHits)
-			lock (actionHits) {
-				try {
-					Log.Info ("Deserializing HistogramRelevanceProvider...");
-					bool isAction;
-					string[] parts;
-					int	key, value;
-					foreach (string line in File.ReadAllLines (DB)) {
-						try {
-							parts = line.Split ('\t');
-							key = int.Parse (parts[0]);
-							value = int.Parse (parts[1]);
-							isAction = parts[2] == "1";
-
-							if (isAction) {
-								actionHits[key] = value;
-								maxActionHits = Math.Max (maxActionHits, value);
-							} else {
-								itemHits[key] = value;
-								maxItemHits = Math.Max (maxItemHits, value);
-							}
-						} catch {
-							continue;
-						}
-					}
-					Log.Info ("Successfully deserialized HistogramRelevanceProvider.");
-				} catch (Exception e) {
-					Log.Error ("Deserializing HistogramRelevanceProvider failed: {0}", e.Message);
-				}
-			}
-		}
-		
-		protected void Serialize ()
-		{
-			lock (itemHits)
-			lock (actionHits) {
-				try {
-					Log.Info ("Serializing HistogramRelevanceProvider...");
-					using (StreamWriter writer = new StreamWriter (DB)) {
-						// Serialize item hits information:
-						foreach (KeyValuePair<int, int> kvp in itemHits) {
-							writer.WriteLine (string.Format ("{0}\t{1}\t0", kvp.Key, kvp.Value));
-						}
-						// Serialize action hits information:
-						foreach (KeyValuePair<int, int> kvp in actionHits) {
-							writer.WriteLine (string.Format ("{0}\t{1}\t1", kvp.Key, kvp.Value));
-						}
-					}
-					Log.Info ("Successfully serialized HistogramRelevanceProvider.");
-				} catch (Exception e) {
-					Log.Error ("Serializing HistogramRelevanceProvider failed: {0}", e.Message);
-				}
-			}
-		}
-
-		public override void IncreaseRelevance (DoObject r, string match, DoObject other)
-		{
-			int rel;
-			int key = r.GetHashCode ();
-			Dictionary<int, int> hits;
-			int maxHits;
-			
-			if (r is DoTextItem) return;
-
-			if (!(r is IAction)) {
-				hits = itemHits;
-				maxHits = maxItemHits;
-			} else {
-				hits = actionHits;
-				maxHits = maxActionHits;
-			}
-
-			lock (hits) {
-				hits.TryGetValue (key, out rel);
-				rel = rel + 1;
-				hits[key] = rel;
-				maxHits = Math.Max (maxHits, rel);
-			}
-
-			if (!(r is IAction))
-				maxItemHits = maxHits;
-			else
-				maxActionHits = maxHits;
-		}
-		
-		public override void DecreaseRelevance (DoObject r, string match, DoObject other)
-		{
-			int rel;
-			int key = r.GetHashCode ();
-			Dictionary<int, int> hits;
-			
-			if (r is DoTextItem) return;
-
-			if (!(r is IAction)) {
-				hits = itemHits;
-			} else {
-				hits = actionHits;
-			}
-
-			lock (hits) {
-				hits.TryGetValue (key, out rel);
-				rel = rel - 1;
-				if (rel == 0) {
-					hits.Remove (key);
-				} else {
-					hits[key] = rel;
-				}
-			}
-		}
-		
-		public override float GetRelevance (DoObject r, string match, DoObject other)
-		{
-			float relevance, score, itemReward; // These should all be between 0 and 1.
-			int key = r.GetHashCode ();
-			Dictionary<int, int> hits;
-			int objectHits, maxHits;
-
-			score = base.GetRelevance (r, match, other);
-			if (score == 0) return 0;
-
-			if (!(r is IAction)) {
-				hits = itemHits;
-				maxHits = maxItemHits;
-				itemReward = 1f;
-			} else {
-				hits = actionHits;
-				maxHits = maxActionHits;
-				itemReward = 0f;
-			}
-
-			lock (hits) {
-				hits.TryGetValue (key, out objectHits);
-			}
-			if (objectHits == 0)
-				relevance = 0f;
-			else
-				relevance = (float) objectHits / (float) maxHits;
-
-			/*
-			if (match == "")
-			Console.WriteLine ("{0}: {1} has relevance {2} ({3}/{4}) and score {5}.",
-				match, r, relevance, objectHits, maxHits, score);
-			*/
-
-			return itemReward * .20f +
-				   relevance  * .30f +
-			       score      * .70f;
 		}
 	}
 }
