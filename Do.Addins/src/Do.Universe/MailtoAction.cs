e674f8fa97964b02e17f68171b583cb7f331eba8
David Siegel <dave@x>
2008-05-26 09:55:39 -0500

    Alex's OpenURL patch to support captial characters, and his MailtoAction patch to support contact detail items.

--- Do.Addins/src/Do.Universe/MailtoAction.cs
+++ Do.Addins/src/Do.Universe/MailtoAction.cs
@@ -1,21 +1,23 @@
-//  MailtoAction.cs
-//
-//  GNOME Do is the legal property of its developers, whose names are too numerous
-//  to list here.  Please refer to the COPYRIGHT file distributed with this
-//  source distribution.
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * MailtoAction.cs
+ * 
+ * GNOME Do is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 using System;
 using Mono.Unix;
@@ -46,16 +48,22 @@ namespace Do.Universe
 			get {
 				return new Type[] {
 					typeof (ContactItem),
+					typeof (IContactDetailItem),
 				};
 			}
 		}
 
 		public override bool SupportsItem (IItem item)
 		{
-			ContactItem contact = item as ContactItem;
-			foreach (string detail in contact.Details)
-				if (detail.StartsWith ("email"))
-					return true;
+			if (item is ContactItem) {
+				ContactItem contact = item as ContactItem;
+				foreach (string detail in contact.Details)
+					if (detail.StartsWith ("email"))
+						return true;
+			}
+			else if ((item as IContactDetailItem).Key.StartsWith ("email"))
+				return true;
+			
 			return false;
 		}
 		
@@ -65,11 +73,12 @@ namespace Do.Universe
 			
 			to = "";
 			foreach (IItem item in items) {
+				string email;
 				if (item is ContactItem) {
 					ContactItem contact = item as ContactItem;
-					string email = contact["email"];
+					email = contact["email"];
 					
-					if (null == email) {
+					if (email == null) {
 						foreach (string detail in contact.Details) {
 							if (detail.StartsWith ("email")) {
 								email = contact[detail];
@@ -77,8 +86,10 @@ namespace Do.Universe
 							}
 						}
 					}
-					to += email + ",";
 				}
+				else
+					email = (item as IContactDetailItem).Value;
+				to += email + ",";
 			}
 			Util.Environment.Open ("mailto:" + to);
 			return null;
