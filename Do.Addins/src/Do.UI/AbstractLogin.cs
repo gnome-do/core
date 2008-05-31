5d21e4c30e3dae5b621b5949b93ddfaecd4b68b5
Alex Launi <alex@eriktorvaldsonn>
2008-05-31 01:33:35 -0400

    Clean up AbstractLogin class

--- Do.Addins/src/Do.UI/AbstractLogin.cs
+++ Do.Addins/src/Do.UI/AbstractLogin.cs
@@ -11,41 +11,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theprotected Gtk.Entry Username {
-			get { 
-				return username_entry;
-			}
-		}
-		
-		protected Gtk.Entry Password {
-			get {
-				return password_entry;
-			}
-		}
-		
-		protected Gtk.Label StatusLabel {
-			get {
-				return validate_lbl;
-			}
-		}
-		
-		protected Gtk.Button ValidateButton {
-			get {
-				return validate_btn;
-			}
-		}
-		
-		protected Gtk.Label GetAccountLabel {
-			get {
-				return get_account_lbl;
-			}
-		}
-		
-		protected Gtk.LinkButton GetAccountButton {
-			get {
-				return link_btn;
-			}
-		}
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -59,20 +25,19 @@ using Gtk;
 namespace Do.Addins
 {
 	public abstract partial class AbstractLogin : Gtk.Bin
-	{
+	{		
 		private LinkButton newAcct_btn;
-		
 		public AbstractLogin()
 		{
 			this.Build();
-			 
-			newAcct_btn = new LinkButton ("", "Get an Account");
+			newAcct_btn = new LinkButton ("", "Get account");
 			this.hbox1.Add (newAcct_btn);
 			Gtk.Box.BoxChild wInt = ((Gtk.Box.BoxChild)(this.hbox1[this.newAcct_btn]));
 			wInt.Position = 1;
+			
 			this.ShowAll ();
 		}
-
+		
 		protected Gtk.Entry Username {
 			get { 
 				return username_entry;
@@ -107,9 +72,9 @@ namespace Do.Addins
 			get {
 				return newAcct_btn;
 			}
-		}		
+		}
 
-		protected virtual void OnValidateButtonClicked (object sender, System.EventArgs e)
+		protected virtual void OnValidateBtnClicked (object sender, System.EventArgs e)
 		{
 			Validate ();
 		}
