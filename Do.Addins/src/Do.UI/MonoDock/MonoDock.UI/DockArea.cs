d33301244499727c9ff626dd6c5c9eaaa334109e
Jason Smith <jassmith@gmail.com>
2008-11-14 00:48:48 -0500

    fix build and add some temporary debugging

--- Do.Addins/src/Do.UI/MonoDock/MonoDock.UI/DockArea.cs
+++ Do.Addins/src/Do.UI/MonoDock/MonoDock.UI/DockArea.cs
@@ -266,6 +266,8 @@ namespace MonoDock.UI
 
 		protected override bool OnLeaveNotifyEvent (Gdk.EventCrossing evnt)
 		{
+			Console.WriteLine (evnt.X);
+			Console.WriteLine (evnt.Y);
 			Cursor = new Gdk.Point (Cursor.X, -1);
 			return base.OnLeaveNotifyEvent (evnt);
 		}
