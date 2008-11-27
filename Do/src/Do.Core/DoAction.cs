9d34087bb28a22075d24338a0c7484e03ee81c18
David Siegel <david@x>
2008-11-26 20:00:20 -0800

    We don't need these methods (yet).

--- Do/src/Do.Core/DoAction.cs
+++ Do/src/Do.Core/DoAction.cs
@@ -1,22 +1,21 @@
-/* DoAction.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this
- * source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// DoAction.cs
+//
+// GNOME Do is the legal property of its developers. Please refer to the
+// COPYRIGHT file distributed with this
+// source distribution.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Linq;
@@ -46,15 +45,6 @@ namespace Do.Core {
 		public static IAction Wrap (IAction a)
 		{
 			return a is DoAction ? a : new DoAction (a);
-		}
-
-		public static IAction Unwrap (IAction o)
-		{
-			while (o is DoAction)
-				// We do a traditional cast to throw a cast exception if the wrong
-				// dynamic type was passed.
-				o = (IAction) (o as DoAction).Inner;
-			return o;
 		}
 
 		IEnumerable<Type> item_types, moditem_types;
--- Do/src/Do.Core/DoItemSource.cs
+++ Do/src/Do.Core/DoItemSource.cs
@@ -1,22 +1,21 @@
-/* DoItemSource.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this
- * source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// DoItemSource.cs
+//
+// GNOME Do is the legal property of its developers. Please refer to the
+// COPYRIGHT file distributed with this
+// source distribution.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Linq;
@@ -31,15 +30,6 @@ namespace Do.Core {
 		public static IItemSource Wrap (IItemSource o)
 		{
 			return o is DoItemSource ? o : new DoItemSource (o);
-		}
-
-		public static IItemSource Unwrap (IItemSource o)
-		{
-			while (o is DoItemSource)
-				// We do a traditional cast to throw a cast exception if the wrong
-				// dynamic type was passed.
-				o = (IItemSource) (o as DoItemSource).Inner;
-			return o;
 		}
 
 		IEnumerable<Type> item_types;
