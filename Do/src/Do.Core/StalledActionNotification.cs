51310cdede5448db9154e7b0131ae6f4bada6669
David Siegel <david@david-desktop>
2008-12-16 16:05:29 -0800

    Removed some cruft.

--- Do/src/Do.Core/StalledActionNotification.cs
+++ Do/src/Do.Core/StalledActionNotification.cs
@@ -1,21 +1,21 @@
-/* StalledActionNotification.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this source distribution.
- *  
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// StalledActionNotification.cs
+//
+// GNOME Do is the legal property of its developers. Please refer to the
+// COPYRIGHT file distributed with this source distribution.
+//  
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 using System;
 
@@ -31,7 +31,6 @@ namespace Do.Core
 	{
 		
 		public StalledActionNotification ()
-			: base ()
 		{
 			Action = Exit;
 		}
@@ -54,7 +53,7 @@ namespace Do.Core
 			get { return Catalog.GetString ("End Now"); }
 		}
 
-		static void Exit ()
+		void Exit ()
 		{
 			Environment.Exit (20);
 		}
