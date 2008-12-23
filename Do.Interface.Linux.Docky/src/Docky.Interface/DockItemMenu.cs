23766cd39bbaa09e51f13f52b43d3decfc83935d
Jason Smith <jassmith@gmail.com>
2008-12-23 18:02:56 -0500

    Make separator vertically symetrical

--- Do.Interface.Linux.Docky/src/Docky.Interface/DockItemMenu.cs
+++ Do.Interface.Linux.Docky/src/Docky.Interface/DockItemMenu.cs
@@ -38,10 +38,15 @@ namespace Docky.Interface
 	{
 		class CustomSeparator : HSeparator
 		{
+			public CustomSeparator () : base ()
+			{
+				HeightRequest = 3;
+			}
+			
 			protected override bool OnExposeEvent (Gdk.EventExpose evnt)
 			{
 				using (Context cr = CairoHelper.Create (GdkWindow)) {
-					cr.Rectangle (evnt.Area.X, evnt.Area.Y, evnt.Area.Width, 1);
+					cr.Rectangle (evnt.Area.X, evnt.Area.Y + 1, evnt.Area.Width, 1);
 					cr.Color = new Cairo.Color (.8, .8, .8, .7);
 					cr.Fill ();
 					
