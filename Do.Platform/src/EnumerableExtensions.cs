c7505178812a261860eb4772fba557ed46cca089
David Siegel <david@x>
2008-12-28 16:21:24 -0800

    Documentation for EnumerableExtensions.

--- Do.Platform/src/EnumerableExtensions.cs
+++ Do.Platform/src/EnumerableExtensions.cs
@@ -12,6 +12,20 @@ namespace Do
 {
 	public static class EnumerableExtensions
 	{
+		/// <summary>
+		/// Finds the index of the first element satsifying the given predicate.
+		/// TODO: remove this
+		/// </summary>
+		/// <param name="self">
+		/// A <see cref="IEnumerable"/> to search.
+		/// </param>
+		/// <param name="p">
+		/// A <see cref="Func"/> predicate.
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.Int32"/> index of the first element satisfying p, or -1
+		/// is no element satisfies p.
+		/// </returns>
 		public static int FindIndex<T> (this IEnumerable<T> self, Func<T, bool> p)
 		{
 			int index = 0;
@@ -21,7 +35,19 @@ namespace Do
 			}
 			return -1;
 		}
-		
+
+		/// <summary>
+		/// Performs the specified action on each member of an enumerable.
+		/// </summary>
+		/// <param name="self">
+		/// A <see cref="IEnumerable"/> whose members will have an action performed on them.
+		/// </param>
+		/// <param name="f">
+		/// A <see cref="Action"/> to perform on each member.
+		/// </param>
+		/// <returns>
+		/// The original <see cref="IEnumerable"/> for chaining.
+		/// </returns>
 		public static IEnumerable<T> ForEach<T> (this IEnumerable<T> self, Action<T> f)
 		{
 			foreach (T x in self)
@@ -29,33 +55,28 @@ namespace Do
 			return self;
 		}
 
-		// TODO: move this to ObjectExtensions.cs (?)
-		public static IEnumerable<T> Cons<T> (this T t, IEnumerable<T> ts)
+		/// <summary>
+		/// Prepends ("cons") an element to an enumerable.
+		/// </summary>
+		/// <remarks>
+		/// If maybeTs is null, a singleton enumerable is returned.
+		/// </remarks>
+		/// <param name="t">
+		/// A <see cref="T"/> to prepend.
+		/// </param>
+		/// <param name="ts">
+		/// A <see cref="IEnumerable"/> that may be null.
+		/// </param>
+		/// <returns>
+		/// A <see cref="IEnumerable"/>
+		/// </returns>
+		public static IEnumerable<T> Cons<T> (this T t, IEnumerable<T> maybeTs)
 		{
+			if (t == null) throw new ArgumentNullException ("t");
+			
 			yield return t;
-
-			if (ts != null)
-				foreach (T x in ts)
-					yield return x;
-		}
-
-		public static IEnumerable<T> ToSafeEnumerable<T> (this IEnumerable<T> self)
-			where T : class
-		{
-			IEnumerator enumerator = self.GetEnumerator ();
-			while (true) {
-				T current;
-				try {
-					if (enumerator.MoveNext ())
-						current = enumerator.Current as T;
-					else break;
-				} catch (InvalidOperationException e) {
-					throw e;
-				} catch (Exception) {
-					continue;
-				}
-				yield return current;
-			}
+			if (maybeTs == null) yield break;
+			foreach (T x in maybeTs) yield return x;
 		}
 
 	}
