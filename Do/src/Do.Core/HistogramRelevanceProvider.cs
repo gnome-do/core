8d504393f04aabcc0bd9ce96966de19223a6f302
David Siegel <djsiegel@gmail.com>
2009-01-08 17:03:33 -0800

    More subtle relevance adjustments for name length.

--- Do/src/Do.Core/HistogramRelevanceProvider.cs
+++ Do/src/Do.Core/HistogramRelevanceProvider.cs
@@ -37,6 +37,7 @@ namespace Do.Core {
 
 		const float DefaultRelevance = 0.001f;
 		const float DefaultAge = 1f;
+		const int MaxNameLength = 50;
 
 		static readonly IEnumerable<Type> RewardedItemTypes = new [] {
 			typeof (IApplicationItem),
@@ -111,7 +112,8 @@ namespace Do.Core {
 			// relevance, the object with the shorter name comes first. Objects
 			// with shorter names tend to be simpler, and more often what the
 			// user wants (e.g. "Jay-Z" vs "Jay-Z feat. The Roots").
-			relevance = DefaultRelevance / Math.Max (1, name.Length);
+			float lengthScale = Math.Min (name.Length, MaxNameLength) / MaxNameLength;
+			relevance = DefaultRelevance + DefaultRelevance / 10 * lengthScale;
 
 			if (0 < rec.Hits) {
 				// On a scale of 0 (new) to 1 (old), how old is the item?
