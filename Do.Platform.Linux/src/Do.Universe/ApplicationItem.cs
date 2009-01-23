de2bd349dcb52be2c78a44a6bc590b338106f868
David Siegel <djsiegel@gmail.com>
2009-01-23 13:37:27 -0800

    The lynx desktop item is causing a strange exception that we cannot manage to catch.

--- Do.Platform.Linux/src/Do.Universe/ApplicationItem.cs
+++ Do.Platform.Linux/src/Do.Universe/ApplicationItem.cs
@@ -1,22 +1,21 @@
-/* ApplicationItem.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this
- * source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// ApplicationItem.cs
+//
+// GNOME Do is the legal property of its developers. Please refer to the
+// COPYRIGHT file distributed with this source distribution.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 using System;
 using System.IO;
@@ -47,6 +46,12 @@ namespace Do.Universe.Linux {
 		{
 			string key = path;
 			ApplicationItem appItem;
+
+			if (path == null) throw new ArgumentNullException ("path");
+
+			// TODO Remove this manual override for lynx.desktop item that
+			// has been causing a weird bug.
+			if (path.Contains ("lynx.desktop")) return null;
 
 			if (Instances.ContainsKey (key)) {
 					appItem = Instances [key];
@@ -134,39 +139,6 @@ namespace Do.Universe.Linux {
 				return !show_in.Contains ("kde") || 
 					Environment.GetEnvironmentVariable ("KDE_FULL_SESSION") == "true";
 			}
-		}
-		
-		public string[] MimeTypes {
-			get {
-				if (!item.AttrExists ("MimeType")) {
-					return null;
-				}
-				
-				if (!string.IsNullOrEmpty (mimetype))
-					return mimetype.Split (';');
-				
-				string s = item.GetString ("MimeType");
-				if (s.Length >= 1000) {
-					mimetype = ManualMimeParse () ?? item.GetString ("MimeType");
-					return mimetype.Split (';');
-				}
-				return item.GetString ("MimeType").Split (';');
-			}
-		}
-		
-		private string ManualMimeParse ()
-		{
-			StreamReader reader = new StreamReader (item.Location.Replace ("file://",""));
-			while (!reader.EndOfStream) {
-				string s = reader.ReadLine ();
-				if (!s.Trim ().StartsWith ("MimeType"))
-					continue;
-				s = s.Replace ("MimeType=", "");
-				reader.Dispose ();
-				return s.Trim ();
-			}
-			reader.Dispose ();
-			return null;
 		}
 		
 		/// <summary>
