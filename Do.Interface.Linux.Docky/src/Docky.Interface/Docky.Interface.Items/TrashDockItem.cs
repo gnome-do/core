523102fbea9b10487cd9ca0f71d869105c51466a
Jason Smith <jassmith@gmail.com>
2009-02-27 04:52:04 -0500

    Dont watch subdirs

--- Do.Interface.Linux.Docky/src/Docky.Interface/Docky.Interface.Items/TrashDockItem.cs
+++ Do.Interface.Linux.Docky/src/Docky.Interface/Docky.Interface.Items/TrashDockItem.cs
@@ -66,7 +66,7 @@ namespace Docky.Interface
 			SetText (Catalog.GetString ("Trash"));
 			
 			fsw = new FileSystemWatcher (Trash);
-			fsw.IncludeSubdirectories = true;
+			fsw.IncludeSubdirectories = false;
 			fsw.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
 
 			fsw.Changed += HandleChanged;
