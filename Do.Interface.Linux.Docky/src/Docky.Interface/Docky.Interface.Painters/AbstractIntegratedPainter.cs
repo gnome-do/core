54ce257b6c69ff31165dbd8d1a13c7d54a9f92da
Robert Dyer <rdyer@yamuna>
2009-06-05 14:18:57 -0500

    fixing AbstractIntegratedPainter.Clicked point bug

--- Do.Interface.Linux.Docky/src/Docky.Interface/Docky.Interface.Painters/AbstractIntegratedPainter.cs
+++ Do.Interface.Linux.Docky/src/Docky.Interface/Docky.Interface.Painters/AbstractIntegratedPainter.cs
@@ -64,7 +64,7 @@ namespace Docky.Interface.Painters
 		public void Clicked (Gdk.Rectangle dockArea, Gdk.Point cursor)
 		{
 			Gdk.Rectangle paintArea = new Gdk.Rectangle (0, 0, Width, dockArea.Height);
-			Gdk.Point paintAreaCursor = new Gdk.Point (cursor.X - dockArea.Left - DockPreferences.FullIconSize - BorderSize,
+			Gdk.Point paintAreaCursor = new Gdk.Point (cursor.X - dockArea.Left - DockPreferences.FullIconSize - BorderSize - (dockArea.Width - MinimumWidth) / 2,
 			                                           cursor.Y - dockArea.Top);
 
 			if (!paintArea.Contains (paintAreaCursor) || ReceiveClick (paintArea, paintAreaCursor))
