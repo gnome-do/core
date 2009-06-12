48147bbec5a1b2780a548a547aa50102114fd6dc
Chris S. <chris@szikszoy.com>
2009-06-12 11:40:07 -0700

    item checking on CopyToClipboardAction (Do.Platform.Linux)

--- Do.Platform.Linux/src/Do.Universe/CopyToClipboardAction.cs
+++ Do.Platform.Linux/src/Do.Universe/CopyToClipboardAction.cs
@@ -52,14 +52,29 @@ namespace Do.Universe.Linux
 			get { yield return typeof (Item); }
 		}
 		
+		public override bool SupportsItem (Item item)
+		{
+			return !(item is IApplicationItem);
+		}
+		
 		public override IEnumerable<Item> Perform (IEnumerable<Item> items, IEnumerable<Item> modItems)
 		{
 			string text = "";
 			Item item = items.First ();
-
+			
 			if (item is ITextItem)
 				text = (item as ITextItem).Text;
-			else
+			else if (item is IFileItem)
+				text = (item as IFileItem).Path;
+			else if (item is IUriItem)
+				text = (item as IUriItem).Uri;
+			else if (item is IUrlItem)
+				text = (item as IUrlItem).Url;
+			else if (item is IContactDetailItem)
+				text = (item as IContactDetailItem).Value;
+			else if (item is ContactItem)
+				text = (item as ContactItem).Name;
+			else					
 				text = string.Format ("{0} - {1}", item.Name, item.Description);
 			
 			Clipboard.Get (Gdk.Selection.Clipboard).Text =
