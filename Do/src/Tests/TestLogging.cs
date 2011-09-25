9b6759f406c059790ca6e89f2f3e9b3146f2b14d
Christopher James Halse Rogers <raof@ubuntu.com>
2011-09-25 19:12:14 +1000

    Core: Add test infrastructure for Log
    Test it with a simple test of logging :)

---
+++ Do/src/Tests/TestLogging.cs
@@ -0,0 +1,81 @@
+// 
+//  TestLogging.cs
+//  
+//  Author:
+//       Christopher James Halse Rogers <raof@ubuntu.com>
+// 
+//  Copyright © 2011 Christopher James Halse Rogers <raof@ubuntu.com>
+// 
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation; either version 2.1 of the
+//  License, or (at your option) any later version.
+// 
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Mono.Addins;
+using Do.Platform;
+using Do.Platform.Common;
+using Do.Platform.ServiceStack;
+
+namespace Do
+{
+	public class MockLogger : AbstractLogService
+	{
+		List<Tuple<LogLevel, string>> log;
+
+		public override void Log (LogLevel level, string msg)
+		{
+			if (log != null)
+				log.Add (new Tuple<LogLevel, string> (level, msg));
+		}
+
+		public void StartLog ()
+		{
+			log = new List<Tuple<LogLevel, string>> ();
+		}
+
+		public IEnumerable<Tuple<LogLevel, string>> EndLog ()
+		{
+			return Interlocked.Exchange (ref log, null);
+		}
+	}
+
+	[TestFixture()]
+	public class TestLogging
+	{
+		MockLogger logger;
+		[SetUp()]
+		public void SetUp ()
+		{
+			Gtk.Application.Init ();
+			Gdk.Threads.Init ();
+			Core.PluginManager.Initialize ();
+			AddinManager.Registry.Update ();
+			logger = Services.Logs.OfType<MockLogger> ().First ();
+		}
+
+		[Test()]
+		public void TestSimpleDebugLogIsLogged ()
+		{
+			logger.StartLog ();
+			Log.Debug ("This is a log message");
+			var logs = logger.EndLog ();
+			Assert.Contains (new Tuple<LogLevel, string> (LogLevel.Debug, "This is a log message"), logs.ToArray ());
+		}
+	}
+}
+
